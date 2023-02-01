d159e2af9ae0541216b816b9c72a349c7e0431cd
Kathrin Ungru <kathrin.ungru@fh-muenster.de>
2023-02-01 13:02:36 +0100

    show framerate not milliseconds
    (cherry picked from commit 792c109930e6665fd46f2d439e3946fc4fd46f1c)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Scene } from "./1_scene/scene";
 
-import Stats = require("stats.js");
+import * as Stats from "stats.js";
 
 const stats = new Stats();
-stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
 
 const scene = new Scene();
 
