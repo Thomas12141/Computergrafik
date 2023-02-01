6f7a115efd89338740edf19c2f814c6f7eba7378
Kathrin Ungru <kathrin.ungru@fh-muenster.de>
2023-02-01 12:41:09 +0100

    add stats.js package and show framerates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { Scene } from "./1_scene/scene";
 
+import Stats = require("stats.js");
+
+const stats = new Stats();
+stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
 const scene = new Scene();
 
 function drawAnimatedScene() {
+    stats.begin();
     scene.draw();
+    stats.end();
+
     requestAnimationFrame(drawAnimatedScene);
 }
 
